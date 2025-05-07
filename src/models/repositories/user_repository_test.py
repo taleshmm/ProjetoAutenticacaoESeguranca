98d9ed0e3f50963e4aa98cc4862be3e389d99c97
taleshmm <taleshmm@hotmail.com>
2025-05-07 19:26:48 -0300

    feat: implementing users repository

---
+++ src/models/repositories/user_repository_test.py
@@ -0,0 +1,16 @@
+from .user_repository import UserRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+def test_repository():
+    db_connection_handler.connect()
+    conn = db_connection_handler.get_connection()
+    repo = UserRepository(conn)
+    
+    username = "Magik"
+    password = "Iron432%"
+    
+    repo.registry_user(username, password)
+    user = repo.get_user_by_username(username)
+    print()
+    print(user)
+    
