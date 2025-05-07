0de8f9246c948a2345753025f60108b4084ed531
taleshmm <taleshmm@hotmail.com>
2025-05-07 20:25:18 -0300

    feat: implementing PasswordHandler

---
+++ src/drivers/password_handler_test.py
@@ -0,0 +1,13 @@
+from .password_handler import PasswordHandler
+
+def test_encrypt():
+    minha_senha = "123Learning%$"
+    password_handler = PasswordHandler()
+    
+    hashed_password = password_handler.encrypt_password(minha_senha)
+    password_checked = password_handler.check_password(minha_senha, hashed_password)
+    
+    assert password_checked
+    
+    
+    
