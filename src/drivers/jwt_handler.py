fd918da9bab6e835c9d95e4ed4e896765ca9a1cf
taleshmm <taleshmm@hotmail.com>
2025-05-08 16:25:57 -0300

    feat: implementing configs

--- src/drivers/jwt_handler.py
+++ src/drivers/jwt_handler.py
@@ -1,22 +1,23 @@
 import jwt
 from datetime import datetime, timedelta, timezone
+from src.configs.jwt_configs import jwt_infos
       
 class JwtHandler:
     def create_jwt_token(self, body: dict = {}) -> str:
        token = jwt.encode(
             payload={
-                "exp": datetime.now(timezone.utc) + timedelta(minutes=10),
+                "exp": datetime.now(timezone.utc) + timedelta(hours=int(jwt_infos["JWT_HOURS"])),
                 **body
             },
-            key="myKey",
-            algorithm="HS256"
+            key=jwt_infos["KEY"],
+            algorithm=jwt_infos["ALGORITHM"]
         )
        
        return token
    
     def decode_jwt_token(self, token: str) -> dict:
        token_information= jwt.decode(
-           token, key="myKey", algorithms="HS256"
+           token, key=jwt_infos["KEY"], algorithms=jwt_infos["ALGORITHM"]
        )
        
        return token_information
